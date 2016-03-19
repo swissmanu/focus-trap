16ec697ef2d4e925fc2db4823603a44a53a8a749
David Clark <david.dave.clark@gmail.com>
2016-03-19 12:59:27 -0700

    Update demo

--- demo/demo-bundle.js
+++ demo/demo-bundle.js
@@ -49,6 +49,7 @@ var el = document.getElementById('demo-three');
 document.getElementById('activate-three').addEventListener('click', function() {
   focusTrap.activate(el, {
     onDeactivate: removeActiveClass,
+    clickOutsideDeactivates: true,
   });
   el.className = 'trap is-active';
 });
@@ -71,6 +72,7 @@ document.getElementById('activate-two').addEventListener('click', function() {
   focusTrap.activate('#demo-two', {
     onDeactivate: removeActiveClass,
     initialFocus: '#focused-input',
+    escapeDeactivates: false,
   });
   el.className = 'trap is-active';
 });
@@ -107,7 +109,10 @@ function activate(element, options) {
   trap = (typeof element === 'string')
     ? document.querySelector(element)
     : element;
+
   config = options || {};
+  if (config.escapeDeactivates === undefined) config.escapeDeactivates = true;
+
   previouslyFocused = document.activeElement;
 
   updateTabbableNodes();
@@ -158,8 +163,13 @@ function deactivate() {
 
 function checkClick(e) {
   if (trap.contains(e.target)) return;
-  e.preventDefault();
-  e.stopImmediatePropagation();
+  if (config.clickOutsideDeactivates) {
+    deactivate();
+    e.target.focus();
+  } else {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
 }
 
 function checkFocus(e) {
@@ -173,7 +183,7 @@ function checkKey(e) {
     handleTab(e);
   }
 
-  if (e.key === 'Escape' || e.key === 'Esc' || e.keyCode === 27) {
+  if (config.escapeDeactivates && isEscapeEvent(e)) {
     deactivate();
   }
 }
@@ -209,6 +219,10 @@ function tryFocus(node) {
   if (node.tagName.toLowerCase() === 'input') {
     node.select();
   }
+}
+
+function isEscapeEvent(e) {
+  return e.key === 'Escape' || e.key === 'Esc' || e.keyCode === 27;
 }
 
 module.exports = {
